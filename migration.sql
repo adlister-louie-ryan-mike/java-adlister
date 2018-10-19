84e984bef3e52bc84524753ecec09f746b265852
ryanschmid <schmid.ryan.c@gmail.com>
2018-10-19 10:41:08 -0500

    Updated MySQL table design for future implementations

--- migration.sql
+++ migration.sql
@@ -1,23 +1,42 @@
+drop database if exists ad_lister_db;
 create database if not exists adlister_db;
-USE adlister_db;
+use adlister_db;
 
-DROP TABLE IF EXISTS ads;
 DROP TABLE IF EXISTS users;
-
 CREATE TABLE users (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     username VARCHAR(240) NOT NULL,
     email VARCHAR(240) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (user_id)
 );
 
+DROP TABLE IF EXISTS ads;
 CREATE TABLE ads (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    ad_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     user_id INT UNSIGNED NOT NULL,
     title VARCHAR(240) NOT NULL,
     description TEXT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    PRIMARY KEY (ad_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
         ON DELETE CASCADE
 );
+
+DROP TABLE IF EXISTS categories;
+CREATE TABLE IF NOT EXISTS categories (
+    category_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    category_desc VARCHAR(50),
+    PRIMARY KEY (category_id)
+);
+
+
+DROP TABLE IF EXISTS ads_categories;
+CREATE TABLE IF NOT EXISTS ads_categories (
+    ad_id INT UNSIGNED NOT NULL,
+    category_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (ad_id) REFERENCES ads (ad_id),
+    FOREIGN KEY (category_id) REFERENCES categories (category_id)
+);
+
+
+
