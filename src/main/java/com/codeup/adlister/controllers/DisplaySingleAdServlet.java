5e0d2947cf12ca454c59d450ea8aab517d9cee07
ryanschmid <schmid.ryan.c@gmail.com>
2018-10-22 14:07:49 -0500

    version prior to Monday AM

--- src/main/java/com/codeup/adlister/controllers/DisplaySingleAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/DisplaySingleAdServlet.java
@@ -1,6 +1,7 @@
 package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,7 +13,11 @@ import java.io.IOException;
 @WebServlet(name = "controllers.AdsIndexServlet", urlPatterns = "/ads/ad")
 public class DisplaySingleAdServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //request.setAttribute("ads", DaoFactory.getAdsDao().all());
+        request.setAttribute("ads", DaoFactory.getAdsDao().all());
+//        String param1 = request.getParameter("id");
+//        Long ad_id = Long.parseLong(param1);
+//        Ad ad = DaoFactory.getAdsDao().getFromID(ad_id);
+//        System.out.println(param1);
         request.getRequestDispatcher("/WEB-INF/ads/ad.jsp").forward(request, response);
     }
 }
